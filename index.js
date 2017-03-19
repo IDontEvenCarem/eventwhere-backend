ffc5515a175743f2ae98807714009fd56fc9321c
IDontEvenCarem <flash3ch@gmail.com>
2017-03-19 22:50:33 +0100

    Changed hashing code
    dd

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var express = require("express");
 var app = express();
 var crypto = require("crypto");
@@ -40,11 +41,14 @@ function connectToMongo() {
     });
     var EventModel = mongoose.model("Eventv1", EventSchema);
 }
-
 function hashPassword(password) {
     var salt = crypto.randomBytes(128).toString('base64');
     var iterations = 10000;
-    var hash = crypto.pbkdf2(password, salt, iterations);
+    let hash;
+    crypto.pbkdf2(password, salt, iterations, '512', 'sha512', (err, key)=>{
+        if (err) throw err;
+        hash = key;
+    });
 
     return {
         salt: salt,
@@ -52,37 +56,42 @@ function hashPassword(password) {
         iterations: iterations
     };
 }
-
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname+'/html/index.html'))
-})
-
+});
 app.post('/api/login', function(req, res){
     var data = req.body;
     var usr = UserModel.find({email: data['uc-email']});
     var pswdh = usr.passwordHash;
     var paswd = data['uc-password'];
     var sid = usr.email+"*"+crypto.randomBytes(128).toString('base64');
-    if(pswdh == crypto.pbkdf2(paswd, usr.salt, urs.iterations)){
-        sessions.push({sid: sid, time: Date.now, user: {email: usr.email, phash: usr.passwordHash}});
-        res.json({status: 'succesfull', sid: sid});
-    }else{
-        res.json({status: 'failed'})
-    }
+    crypto.pbkdf2(paswd, usr.salt, urs.iterations, 512, 'sha512', (err, key)=>{
+        if (err) throw err;
+        if(pswdh == key){
+            sessions.push({sid: sid, time: Date.now, user: {email: usr.email, phash: usr.passwordHash}});
+            res.json({status: 'succesfull', sid: sid});
+        }else{
+            res.json({status: 'failed'});
+        }
+    })
 });
-
 app.post('/api/register', function(req, res){
     var bd = req.body;
-    var hased = hashPassword(bd['uc-r-password']);
-    var newUser = new UserModel({name: bd['uc-r-nick'], birthdate: bd['uc-r-birthdate'], email: bd['uc-r-email'], isAdmin: false, isVIP: false, passwordHash: hased.hash, salt:hased.salt, iterations: hased.iterations});
-    newUser.save();
+    UserModel.count({email: bd['uc-r-email']}, function(err, count){
+        if(count == 0){
+            var hashed = hashPassword(bd['uc-r-password']);
+            var newUser = new UserModel({name: bd['uc-r-nick'], birthdate: bd['uc-r-birthdate'], email: bd['uc-r-email'], isAdmin: false, isVIP: false, passwordHash: hashed.hash, salt:hashed.salt, iterations: hashed.iterations});
+            newUser.save();
+            res.json({status: 'succesfull'});
+        }else{
+            res.json({status: 'email-in-use'});
+        }
+    })
 })
-
 app.get('/api/createAdmins', function(req, res){
     UserModel.findOneAndUpdate({name: 'IDECm'}, {isAdmin: true, isVIP: true});
     res.send("Done");  
 })
-
 app.listen(3000, function(){
     connectToMongo();
     console.log('running');
